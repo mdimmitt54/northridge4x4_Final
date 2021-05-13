e547358e689947603d2aea79d986a43a79ef6cb6
Matt Dimmitt <79881894+mdimmitt54@users.noreply.github.com>
2021-05-13 08:58:09 -0600

    start

---
+++ __tests__/northridge4x4.test.ts
@@ -0,0 +1,33 @@
+import * as employeeData from "./singInData.json";
+import { NorthridgePage } from "./northridgeObjects";
+const chromedriver = require("chromedriver");
+import { WebDriver, Builder, Capabilities } from "selenium-webdriver";
+const driver:WebDriver = new Builder()
+  .withCapabilities(Capabilities.chrome())
+  .build();
+
+
+describe("Chewy page tests suites", ()=> {
+
+    const page = new NorthridgePage(driver);
+    beforeEach(async () => {
+      await page.navigate();
+    });
+    afterAll(async () => {
+      await page.driver.quit();
+    });
+    
+    test("siginingin returning customer", async () =>{
+      await page.navigate();
+      await page.clickOnAccountButton();
+      await page.signIn();
+      expect(await page.getHeaderText()).toContain(`Hi, ${employeeData.name}!`)
+
+    });
+
+    test("the search field works", async ()=> {
+       await page.doSearch("Dog Food");
+        expect(await page.getresults()).toContain("Dog Food");
+        })
+})
+       
---
+++ __tests__/objectsPage/northridgeObjects.ts
@@ -0,0 +1,93 @@
+import * as employeeData from "../singInData.json"
+
+import {
+    Builder,
+    By,
+    Capabilities,
+    until,
+    WebDriver,
+    WebElement,
+  } from "selenium-webdriver";
+import { Driver } from "selenium-webdriver/chrome";
+
+const fs = require("fs");
+const chromedriver = require("chromedriver");
+
+
+export class NorthridgePage {
+    driver:WebDriver
+    url:string = "https://www.northridge4x4.com/"
+    accountButton: By = By.css("a[title*='your account']")
+    siginunderAccount: By = By.css(".sfw-header-drop-account__sign-in")
+    EmailAddressField: By = By.css("label[for*='username']")
+    PasswordField: By = By.css("label[for*='password']")
+    EmailAddressInput: By = By.name("username")
+    PasswordInput: By = By.name("password")
+    SigninButton: By = By.name("submitForm")
+    hiAccountName: By = By.css(".sfw-header-nav-trigger--account__label")
+    searchField:By = By.css("#search-autocomplete");
+    result:By = By.css(".results-products");
+    brandNavigation:By = By.css("#core-nav-1");
+    dealsNavigation:By = By.css ("#core-nav-2");
+    brandHeader:By = By.xpath("//div[@id='page-content']/h1");
+    dealsItem:By = By.xpath("//div[@id='tns1-item0']/a/div[2]/p")
+
+
+
+    constructor(driver: WebDriver) {
+        this.driver = driver;
+    }
+
+    async navigate(){
+        await this.driver.get(this.url)   
+    }
+
+    async clickOnAccountButton(){
+        await this.driver.wait(until.elementLocated(this.accountButton));
+        await this.driver.findElement(this.accountButton).click();
+        await this.driver.wait(until.elementLocated(this.siginunderAccount));
+        await this.driver.findElement(this.siginunderAccount).click();
+    }
+
+    async signIn(): Promise<void> {
+        await this.driver.wait(until.elementLocated(this.EmailAddressField));
+        await this.driver.findElement(this.EmailAddressField).click();
+        await this.driver.findElement(this.EmailAddressInput).sendKeys(employeeData.EmailAddress);
+        await this.driver.findElement(this.PasswordField).click();
+        await this.driver.findElement(this.PasswordInput).sendKeys(employeeData.Password);
+        await this.driver.findElement(this.SigninButton).click();
+    }
+
+    async doSearch(searchItem:String){
+        await this.driver.wait(until.elementLocated(this.searchField));
+        await this.driver.findElement(this.searchField).sendKeys(`${searchItem}\n`);
+    }
+    async getresults(){
+        await this.driver.wait(until.elementLocated(this.result));
+        return await (this.driver.findElement(this.result)).getText();
+    }
+
+    async getHeaderText() {
+        await this.driver.wait(until.elementLocated(this.hiAccountName));
+        return await (this.driver.findElement(this.hiAccountName)).getText();
+    }
+    async clickOnDealsButton(){
+        await this.driver.wait(until.elementLocated(this.dealsNavigation));
+        await this.driver.findElement(this.dealsNavigation).click();
+        await this.driver.wait(until.elementLocated(this.dealsItem))
+    }
+    async clickOnBrandsButton() {
+        await this.driver.wait(until.elementLocated(this.brandNavigation));
+        await (await this.driver.findElement(this.brandNavigation)).click();
+        await this.driver.wait(until.elementLocated(this.brandHeader))
+    }
+    async getHeaderTextBrand() {
+        await this.driver.wait(until.elementLocated(this.brandHeader));
+        return await (this.driver.findElement(this.brandHeader)).getText();
+    }
+    async getItemText() {
+        await this.driver.wait(until.elementLocated(this.dealsItem));
+        return await (this.driver.findElement(this.dealsItem)).getText();
+}
+       
+}
